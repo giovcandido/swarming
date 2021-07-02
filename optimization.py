1dad1f5d336cbac7ac01d16ef4b3b2fc4a155c40
Giovani Candido <giovcandido@outlook.com>
2021-07-02 16:33:49 -0300

    Add optimization script

---
+++ optimization.py
@@ -0,0 +1,37 @@
+import argparse
+
+from PSO import PSO
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-s', '--swarm-size', help="number of particles in the swarm", required=True)
+parser.add_argument('-m', '--max-iterations', help="maximum number of iterations", required=True)
+
+# Parse swarm size and dimension
+args = parser.parse_args()
+
+# Set the number of particles in the swarm
+swarm_size = int(args.swarm_size)
+
+# Set the dimension of the problem
+dimension = 2
+
+# And the function to be optimized
+function = lambda x: x[0] ** 2 + ((x[1] ** 2) / 16 - 5) ** 2 + 2 * x[0] + 6
+
+# Create a PSO instance
+pso = PSO(swarm_size, dimension, function)
+
+# Set maximum number of iterations
+max_iterations = int(args.max_iterations)
+
+# Find an approximate solution with PSO
+approx_sol = pso.optimize(max_iterations)
+
+print('\nThe approximate solution is:')
+
+for i, x in enumerate(approx_sol):
+    x = round(x, 4) + 0
+    print('x[%i] = %.4f' % (i, x))
+
+print('')
