5853ebf36bfa0908187595eb92690043981b42ff
giovcandido <giovcandido@outlook.com>
2021-07-05 23:21:08 -0300

    Add times to run arg and new info messages

--- optimization.py
+++ optimization.py
@@ -6,6 +6,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('-s', '--swarm-size', help="number of particles in the swarm", required=True)
 parser.add_argument('-m', '--max-iterations', help="maximum number of iterations", required=True)
+parser.add_argument('-t', '--times', help="number of times to run", required=True)
 
 # Parse swarm size and maximum number of iterations
 args = parser.parse_args()
@@ -28,12 +29,16 @@ lower_bounds = (-100, -100)
 upper_bounds = (100, 100)
 
 # Number of times to run the algorithm
-times_to_run = 15
+times_to_run = int(args.times)
 
 # Run the PSO algorithm many times
 # It helps to check if the restricted search space is appropriate
 
-for _ in range(times_to_run):
+print('Running algorithm %i times...\n' % times_to_run)
+
+for i in range(1, times_to_run + 1):
+    print('==> Run number %i' % i)
+
     # Create a PSO instance
     pso = PSO(swarm_size, dimension, function, lower_bounds, upper_bounds)
 
@@ -43,7 +48,7 @@ for _ in range(times_to_run):
     # Find an approximate solution with PSO
     approx_sol = pso.optimize(max_iterations)
 
-    print('\nThe approximate solution is:')
+    print('Approximate solution:')
 
     for i, x in enumerate(approx_sol):
         x = round(x, 4) + 0
