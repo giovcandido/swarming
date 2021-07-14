8ae3fdde6a09d2cedef44e91832d60ed1acc6501
giovcandido <giovcandido@outlook.com>
2021-07-14 12:26:53 -0300

    Change time measurement to include particles init

--- optimization.py
+++ optimization.py
@@ -48,13 +48,13 @@ if __name__ == '__main__':
         # Defines a random seed to achieve constant results
         np.random.seed(i)
 
-        # Create a PSO instance
-        pso = PSO(swarm_size, dimension, function, lower_bounds, upper_bounds)
-
         # Set maximum number of iterations
         max_iterations = int(args.max_iterations)
         
         start_time = time.time()
+
+        # Create a PSO instance
+        pso = PSO(swarm_size, dimension, function, lower_bounds, upper_bounds)
 
         # Find an approximate solution with PSO
         approx_sol, fit = pso.optimize(max_iterations)
