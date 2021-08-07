e6d9a165f815fcdddce518b6c4fc326efb83ac98
Giovani Candido <giovcandido@outlook.com>
2021-08-07 17:59:33 -0300

    Fix parallel help message in the argparse

--- optimization.py
+++ optimization.py
@@ -12,7 +12,7 @@ def function(x):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--parallel', help="number of particles in the swarm", required=True)
+parser.add_argument('-p', '--parallel', help="parallel execution or not", required=True)
 parser.add_argument('-s', '--swarm-size', help="number of particles in the swarm", required=True)
 parser.add_argument('-m', '--max-iterations', help="maximum number of iterations", required=True)
 parser.add_argument('-t', '--times', help="number of times to run", required=True)
