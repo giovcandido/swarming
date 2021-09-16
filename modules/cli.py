9b1180919b07ee28830f94439974123511128b29
giovcandido <giovcandido@outlook.com>
2021-09-16 16:40:37 -0300

    Improve arguments handling by adding choice, type and default

--- modules/cli.py
+++ modules/cli.py
@@ -3,9 +3,13 @@ from argparse import ArgumentParser
 def parse_arguments():
     parser = ArgumentParser()
     
-    parser.add_argument('-p', '--parallel', help="parallel execution or not", required=True)
-    parser.add_argument('-s', '--swarm-size', help="number of particles in the swarm", required=True)
-    parser.add_argument('-m', '--max-iterations', help="maximum number of iterations", required=True)
-    parser.add_argument('-t', '--times', help="number of times to run", required=True)
+    parser.add_argument('-p', '--parallel', metavar='P', choices=['y', 'n'], type=str, 
+                        default='n', help='parallel execution or not')
+    parser.add_argument('-s', '--swarm-size', metavar='S', type=int, 
+                        help='number of particles in the swarm', required=True)
+    parser.add_argument('-i', '--iterations', metavar='I', type=int, 
+                        help='number of iterations', required=True)
+    parser.add_argument('-t', '--times', metavar='T', type=int, 
+                        default=1, help='number of times to run')
 
     return parser.parse_args()
