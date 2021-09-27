d9814a916637965cd83ae609b1f0f998c6b242a1
giovcandido <giovcandido@outlook.com>
2021-09-27 12:03:14 -0300

    Fix PSO class selection

--- examples/math_function.py
+++ examples/math_function.py
@@ -35,7 +35,7 @@ def main():
     args = parse_arguments()
 
     # Select the desired PSO class
-    if not parallel:
+    if not args.parallel:
         PSOClass = PSO
     else:
         PSOClass = ParallelPSO
--- examples/neural_net.py
+++ examples/neural_net.py
@@ -139,7 +139,7 @@ def main():
     args = parse_arguments()
 
     # Select the desired PSO class
-    if not parallel:
+    if not args.parallel:
         PSOClass = PSO
     else:
         PSOClass = ParallelPSO
