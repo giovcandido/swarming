8c6a4164b527889b18e4f114cdf245033ba546a1
giovcandido <giovcandido@outlook.com>
2021-09-27 15:07:30 -0300

    Use f strings and shorten more lines

--- swarming/core/pso.py
+++ swarming/core/pso.py
@@ -15,7 +15,7 @@ logger = Logger.get_logger(__name__)
 
 class PSO:
 
-    def __init__(self, swarm_size, dimension, function, lower_bounds, upper_bounds):
+    def __init__(self, swarm_size, dimension, function, l_bounds, u_bounds):
         logger.info('Creating an instance of the PSO class')
 
         # Set inertia constant
@@ -27,9 +27,9 @@ class PSO:
         # Set social constant
         self._c2 = 1.7
 
-        logger.debug('Inertia Constant = %s' % (self._w))
-        logger.debug('Cognitive Constant = %s' % (self._c1))
-        logger.debug('Social Constant = %s' % (self._c2))
+        logger.debug(f'Inertia Constant = {self._w}')
+        logger.debug(f'Cognitive Constant = {self._c1}')
+        logger.debug(f'Social Constant = {self._c2}')
 
         # Set number of particles in the swarm
         self._swarm_size = swarm_size
@@ -37,25 +37,26 @@ class PSO:
         # Set search space dimension
         self._dimension = dimension
 
-        logger.debug('Swarm size = %s' % (self._swarm_size))
-        logger.debug('Dimension = %s' % (self._dimension))
+        logger.debug(f'Swarm size = {self._swarm_size}')
+        logger.debug(f'Dimension = {self._dimension}')
 
         # Set function to be optimized
         self._function = function
 
-        logger.debug('Function = %s' % (self._function))
+        logger.debug(f'Function = {self._function}')
 
         # Set search space boundaries
-        self._lower_bounds = lower_bounds
-        self._upper_bounds = upper_bounds
+        self._lower_bounds = l_bounds
+        self._upper_bounds = u_bounds
 
-        logger.debug('Lower Bounds = %s' % (self._lower_bounds))
-        logger.debug('Upper Bounds = %s' % (self._upper_bounds))
+        logger.debug(f'Lower Bounds = {self._lower_bounds}')
+        logger.debug(f'Upper Bounds = {self._upper_bounds}')
 
         logger.info('PSO instance was created successfully')
 
     def optimize(self, iterations, executions=1):
-        logger.info('Task has %s executions with %s iterations each' % (executions, iterations))
+        logger.info(f'Task has {executions} executions')
+        logger.info(f'With {iterations} iterations each execution')
 
         # Create lists to save output from multiple executions
         positions, scores = [], []
@@ -66,9 +67,9 @@ class PSO:
             # Set a random seed to achieve constant results
             np.random.seed(i)
 
-            logger.debug('Random Seed = %s' % (i))
+            logger.debug(f'Random Seed = {i}')
 
-            logger.info('Execution %s is in progress' % (i + 1))
+            logger.info(f'Execution {i + 1} is in progress')
 
             start_time = time()
 
@@ -77,9 +78,9 @@ class PSO:
 
             end_time = time()
 
-            logger.info('Execution %s ended successfully' % (i + 1))
+            logger.info(f'Execution {i + 1} ended successfully')
 
-            logger.info('It took %s s' % (end_time - start_time))
+            logger.info(f'It took {end_time - start_time} s')
 
             # Save best position from the current execution
             positions.append(curr_position)
@@ -87,32 +88,37 @@ class PSO:
             # Save best score from the current execution
             scores.append(curr_score)
 
-            logger.info('Execution best position = %s' % (curr_position))
-            logger.info('Execution Best score = %s' % (curr_score))
+            logger.info(f'Execution best position = {curr_position}')
+            logger.info(f'Execution Best score = {curr_score}')
 
         best_score_index = np.argmin(scores)
 
-        logger.info('Execution %s had the best score' % (best_score_index + 1))
+        logger.info(f'Execution {best_score_index + 1} had the best score')
 
-        logger.info('Task best position = %s' % (positions[best_score_index]))
-        logger.info('Task best score = %s' % (scores[best_score_index]))
+        logger.info(f'Best task position = '
+                    f'{positions[best_score_index]}')
+
+        logger.info(f'Best task score = '
+                    f'{scores[best_score_index]}')
 
         # Save task best position
-        np.save('task_best_position-' + strftime("%Y%m%d-%H%M%S"), positions[best_score_index])
+        output_filename = 'task_best_position-' + strftime('%Y%m%d-%H%M%S')
+        np.save(output_filename, positions[best_score_index])
 
     def _execute(self, iterations):
-        logger.info('Initializing the search space with %s particles' % (self._swarm_size))
+        logger.info(f'Creating the search space '
+                    f'with {self._swarm_size} particles')
 
         # Initialize the search space
         self._initialize_search_space()
 
         logger.info('Search space was initialized successfully')
 
-        logger.info('Moving particles around the search space for %s iterations' % (iterations))
+        logger.info('Moving particles around the search space')
 
         # Move particles up to the maximum number of iterations
         for i in tqdm(range(iterations)):
-            logger.write('Iteration %s/%s' % (i + 1, iterations))
+            logger.write(f'Iteration {i + 1}/{iterations}')
 
             # Loop over all particles in the swarm
             for particle in self._swarm:
@@ -126,8 +132,11 @@ class PSO:
                 if score < particle.best_score:
                     self._update_best_position(particle, score)
 
-            logger.write('Iteration best position = %s' % (self._best_swarm_position))
-            logger.write('Iteration best score = %s' % (self._best_swarm_score))
+            logger.write(f'Best iteration position = '
+                         f'{self._best_swarm_position}')
+
+            logger.write(f'Best iteration score = '
+                         f'{self._best_swarm_score}')
 
         # Return the best swarm position as an approximate solution
         return self._best_swarm_position, self._best_swarm_score
@@ -138,7 +147,8 @@ class PSO:
         # Initialize the particles in the swarm
         for i in tqdm(range(self._swarm_size)):
             # Create an instance of the particle class
-            particle = Particle(self._dimension, self._lower_bounds, self._upper_bounds)
+            particle = Particle(self._dimension,
+                                self._lower_bounds, self._upper_bounds)
 
             # Calculate particle score
             particle.best_score = self._function(particle.best_position)
@@ -164,10 +174,14 @@ class PSO:
         inertia_factor = self._w * particle.velocity
 
         # Calculate particle best position influence
-        cognitive_factor = self._c1 * r1 * (particle.best_position - particle.position)
+        cognitive_factor = (self._c1
+                            * r1
+                            * (particle.best_position - particle.position))
 
         # Calculate swarm best position influence
-        social_factor = self._c2 * r2 * (self._best_swarm_position - particle.position)
+        social_factor = (self._c2
+                         * r2
+                         * (self._best_swarm_position - particle.position))
 
         # Add all three factors to get the new velocity
         particle.velocity = inertia_factor + cognitive_factor + social_factor
@@ -175,8 +189,9 @@ class PSO:
         # Add current position and velocity to get the new position
         particle.position = particle.position + particle.velocity
 
-        # Clip the new position, so that the particle stays within the search space bounds
-        particle.position = np.clip(particle.position, self._lower_bounds, self._upper_bounds)
+        # Clip the new position, so that the particle stays within the bounds
+        particle.position = np.clip(particle.position,
+                                    self._lower_bounds, self._upper_bounds)
 
     def _update_best_position(self, particle, score):
         particle.best_position = particle.position
@@ -193,15 +208,15 @@ class PSO:
 
 class ParallelPSO(PSO):
 
-    def __init__(self, swarm_size, dimension, function, lower_bounds, upper_bounds):
-        PSO.__init__(self, swarm_size, dimension, function, lower_bounds, upper_bounds)
+    def __init__(self, swarm_size, dimension, function, l_bounds, u_bounds):
+        PSO.__init__(self, swarm_size, dimension, function, l_bounds, u_bounds)
 
         logger.info('Specializing instance as one of the ParallelPSO class')
 
         # Get number of cpus available including logical threads
         num_cpus = cpu_count(logical=True)
 
-        logger.debug('Threads = %s' % (num_cpus))
+        logger.debug(f'Threads = {num_cpus}')
 
         # Initialize ray instance
         ray.init(num_cpus=num_cpus, logging_level=FATAL)
@@ -211,30 +226,32 @@ class ParallelPSO(PSO):
         # Save function as ray remote
         self._function = ray.remote(function)
 
-        logger.debug('Function = %s' % (self._function))
+        logger.debug(f'Function = {self._function}')
 
         logger.info('ParallelPSO instance was created successfully')
 
     def _execute(self, iterations):
-        logger.info('Initializing the search space with %s particles' % (self._swarm_size))
+        logger.info(f'Creating the search space '
+                    f'with {self._swarm_size} particles')
 
         # Initialize the search space
         self._initialize_search_space()
 
         logger.info('Search space was initialized successfully')
 
-        logger.info('Moving particles around the search space for %s iterations' % (iterations))
+        logger.info(f'Moving particles for {iterations} iterations')
 
         # Move particles up to the maximum number of iterations
         for i in tqdm(range(iterations)):
-            logger.write('Iteration %s/%s' % (i + 1, iterations))
+            logger.write(f'Iteration {i + 1}/{iterations}')
 
             # Loop over all particles in the swarm
             for particle in self._swarm:
                 # Update velocity and position
                 self._update(particle)
 
-            scores = ray.get([self._function.remote(p.position) for p in self._swarm])
+            scores = ray.get(
+                [self._function.remote(p.position) for p in self._swarm])
 
             # Loop over all particles in the swarm
             for i, particle in enumerate(self._swarm):
@@ -242,8 +259,11 @@ class ParallelPSO(PSO):
                 if scores[i] < particle.best_score:
                     self._update_best_position(particle, scores[i])
 
-            logger.write('Iteration best position = %s' % (self._best_swarm_position))
-            logger.write('Iteration best score = %s' % (self._best_swarm_score))
+            logger.write(f'Best iteration position = '
+                         f'{self._best_swarm_position}')
+
+            logger.write(f'Best iteration score = '
+                         f'{self._best_swarm_score}')
 
         # Return the best swarm position as an approximate solution
         return self._best_swarm_position, self._best_swarm_score
@@ -253,11 +273,13 @@ class ParallelPSO(PSO):
 
         # Initialize the particles in the swarm
         for i in range(self._swarm_size):
-            particle = Particle(self._dimension, self._lower_bounds, self._upper_bounds)
+            particle = Particle(self._dimension,
+                                self._lower_bounds, self._upper_bounds)
 
             self._swarm.append(particle)
 
-        scores = ray.get([self._function.remote(p.best_position) for p in tqdm(self._swarm)])
+        scores = ray.get(
+            [self._function.remote(p.best_position) for p in tqdm(self._swarm)])
 
         # Set best score of the firt particle
         self._swarm[0].best_score = scores[0]
